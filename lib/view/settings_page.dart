812ed3c2602910be79fc85baf904e07c00a407d0
takezawa <s.take4173@gmail.com>
2025-06-09 22:55:08 +0900

    Add settings page

---
+++ lib/view/settings_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class SettingsPage extends StatelessWidget {
+  const SettingsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return WillPopScope(
+      onWillPop: () async => true,
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: const Text('設定'),
+          leading: IconButton(
+            icon: const Icon(Icons.arrow_back),
+            onPressed: () => Navigator.of(context).pop(),
+          ),
+        ),
+        body: const Center(child: Text('設定画面')),
+      ),
+    );
+  }
+}
