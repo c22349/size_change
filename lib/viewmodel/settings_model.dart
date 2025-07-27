e81b3cd2a0670fedbac29a4eb53f043eee197099
takezawa <s.take4173@gmail.com>
2025-07-27 21:26:33 +0900

    Fix build error

--- lib/viewmodel/settings_model.dart
+++ lib/viewmodel/settings_model.dart
@@ -3,37 +3,43 @@ import "package:shared_preferences/shared_preferences.dart";
 import 'dart:io';
 
 class SettingModel with ChangeNotifier {
-  // 初期値
-  final _settingModel = SettingModel();
+  bool _soundEnabled = true;
+  Locale _currentLocale = const Locale('ja', '');
 
-  // 設定の取得
-  Future<SettingModel> getSetting() async {
+  // 初期化メソッド
+  Future<void> init() async {
     final prefs = await SharedPreferences.getInstance();
-    final setting = SettingModel.fromJson(prefs.getString("setting") ?? "{}");
-    return setting;
-  }
-
-  // アプリ起動時に呼び出すメソッド
-  Future<void> loadLocale() async {
-    final prefs = await SharedPreferences.getInstance();
-    String? savedLanguageCode = prefs.getString('language');
-    Locale deviceLocale = getDeviceLocale();
-    String defaultLanguageCode =
-        deviceLocale.languageCode == 'ja' ? 'ja' : 'en';
-    _currentLocale = Locale(savedLanguageCode ?? defaultLanguageCode, '');
-    notifyListeners();
     _soundEnabled = prefs.getBool('soundEnabled') ?? true;
+    String? savedLanguageCode = prefs.getString('language');
+    if (savedLanguageCode != null) {
+      _currentLocale = Locale(savedLanguageCode, '');
+    }
+    notifyListeners();
   }
 
+  // ゲッター
+  bool get soundEnabled => _soundEnabled;
+  Locale get currentLocale => _currentLocale;
+
+  // サウンド設定の更新
   void updateSoundSetting(bool isEnabled) {
     _soundEnabled = isEnabled;
     notifyListeners();
   }
 
-  void setSoundEnabled(bool value) async {
+  // サウンド設定の保存
+  Future<void> setSoundEnabled(bool value) async {
     final prefs = await SharedPreferences.getInstance();
     await prefs.setBool('soundEnabled', value);
     _soundEnabled = value;
+    notifyListeners();
+  }
+
+  // 言語設定の保存
+  Future<void> setLanguage(String languageCode) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString('language', languageCode);
+    _currentLocale = Locale(languageCode, '');
     notifyListeners();
   }
 }
